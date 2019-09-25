f5ad0514857b1360554d43a8c777870cc415289c
John Charles Lionel Reese <j.charles.reese@gmail.com>
2019-09-25 16:31:26 -0400

    Allow egress from web app server instances

--- web_servers/main.tf
+++ web_servers/main.tf
@@ -259,6 +259,16 @@ resource "aws_security_group_rule" "allow_server_ssh_inbound" {
   cidr_blocks = local.all_ips
 }
 
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
 # 6.2 Application load balancer security groups
 
 resource "aws_security_group" "alb" {
