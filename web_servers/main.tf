52ab2df5b39b2a64704f0d199da9bc6deb001e8b
John Charles Lionel Reese <j.charles.reese@gmail.com>
2019-10-02 11:18:19 -0400

    Don't need subject alternative names

--- web_servers/main.tf
+++ web_servers/main.tf
@@ -336,10 +336,9 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
 # ---------------------------------------------------------------------------------------------------------------------
 
 resource "aws_acm_certificate" "cert" {
-  count                     = var.env == "prod" ? 0 : 1
-  domain_name               = "*.${var.domain_name}"
-  subject_alternative_names = ["${var.domain_name}", "*.${var.domain_name}"]
-  validation_method         = "DNS"
+  count             = var.env == "prod" ? 0 : 1
+  domain_name       = "*.${var.domain_name}"
+  validation_method = "DNS"
 
   tags = {
     Environment = var.env
